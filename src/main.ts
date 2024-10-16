3f500e3683fb1c0d86a6dbbca9d5892ecddbf36b
Nate Valley <46508762+NateValley@users.noreply.github.com>
2024-10-16 01:29:53 +0000

    Step 1 Completed!

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,9 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const webTitle = document.createElement("h1");
+app.append(webTitle);
+
+const webCanvas = document.createElement("canvas");
+app.append(webCanvas);
